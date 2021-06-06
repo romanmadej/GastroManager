5fdb4c018094504593b58171fcb436aa5b7944de
Maciej Matys <m.j.matys@gmail.com>
2021-06-06 21:18:18 +0200

    create database directory

--- rules.sql
+++
@@ -1,8 +0,0 @@
-create rule no_order_del as on delete to orders
-    do instead nothing;
-
-create rule no_order_details_del as on delete to order_details
-    do instead nothing;
-
-create rule prevent_uncancelling as on update to orders where old.status = 'cancelled' and new.status != 'cancelled'
-    do instead nothing;
